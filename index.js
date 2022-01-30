832c05420db1cf0d3052474a2be197c06932e363
Sarah Indano <sarah.indano@gmail.com>
2022-01-30 15:52:07 -0500

    commit tree c29420a70fdf45a2f246c7056e7d048ef8aaf9e4

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+main.remove('main')
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = 'victory'
+
+document.body.appendChild(newHeader)
+victory.innerHTML = '<h1>Sarah is the champion</h1>'
